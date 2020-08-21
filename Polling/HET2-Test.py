fe4b0696e5ca0219a9036a97a0f1e973a2559ba9
Tanner Songkakul <tpsongka@ncsu.edu>
2020-08-20 22:12:32 -0400

    Polling Archive
    Archive of polling

---
+++ Polling/HET2-Test.py
@@ -0,0 +1,41 @@
+import unittest
+import het2
+
+het = het2.HET2Device()
+test_array = [0] * 80
+amp_sol = list(range(0,20,2))
+ph_sol = list(range(1,20,2))
+for i in range(0,80):
+    if i%4 == 0:
+        test_array[i+3] = int(i/4)
+print(test_array)
+
+class HET2Tests(unittest.TestCase):
+
+    def test_cnvmodes(self):
+        self.assertEqual(het2.cnv_modes('idle','CA_PH'),'00')
+
+    def test_cnvbias(self):
+        self.assertEqual(het2.cnv_bias(0),'80')
+
+    def test_gencmd(self):
+        self.assertEqual(het.gen_cmd_str(),'00000080140101')
+
+    def test_run(self):
+        het.set_devmode('run')
+        het.set_bias(-1.1)
+        het.set_rtia('200')
+        het.set_period(2)
+        self.assertEqual(het.gen_cmd_str(),'00001012010201')
+
+    # def test_amp_parsing(self):
+    #     het.parse_het(bytearray(test_array),"AMPPH")
+    #     self.assertEqual(amp_sol, het.amp_data)
+    #
+    # def test_ph_parsing(self):
+    #     het.ph_data = []
+    #     het.parse_het(bytearray(test_array),"AMPPH")
+    #     self.assertEqual(ph_sol, het.ph_data)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Polling/het2.py
@@ -0,0 +1,283 @@
+from customperipheral import CustomPeripheral
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QFileDialog
+from ctypes import *
+from os import getcwd
+import csv, time, string
+
+# define constants here
+dev_modes = ['idle', 'run', 'save', 'dump']
+pstat_modes = ['CA_PH', 'CV']
+rtia_values = [
+    'External', '200', '1k', '2k', '3k',
+    '4k', '6k', '8k', '10k', '12k',
+    '16k', '20k', '24k', '30k', '32k',
+    '40k', '48k', '64k', '85k', '96k',
+    '100k', '120k', '128k', '160k', '196k',
+    '256k', '512k'] # this needs to match
+period_values = [
+     0.05, 0.1, 0.125, 0.1667, 0.25,
+     0.5, 1, 2, 2.5, 5,
+     10, 20, 25, 30, 50,
+     60, 120, 150, 300, 600
+    ]
+
+pga_values = [1, 1.5, 2, 4, 9]
+
+
+class HET2Device(CustomPeripheral):  # HET2 class compatible with SW version 0.0
+    # ###
+    # HET2 Object using UUIDs from custom peripheral
+    #
+    # Packet Structures
+    # Char1- System Config Data v(10 Bytes)
+    #
+    #
+    # Char2- Electrochemical Data (81 Bytes)
+    #
+    # AMP-PH Mode
+
+    def __init__(self):
+        super(HET2Device, self).__init__()
+        self.id = 0
+        self.dev_mode = 'idle'
+        self.dev_mode_new = self.dev_mode
+        self.pstat_mode = 'CA_PH'
+        self.pstat_mode_new = self.pstat_mode
+        self.bias = 0 # between -1.27 and 1.27V
+        self.bias_new = self.bias # between -1.27 and 1.27V
+        self.r_tia = '100k'
+        self.r_tia_new = self.r_tia
+        self.period = 1 # index in lookup table
+        self.period_new = self.period
+        self.pga_gain = 1.5
+        self.pga_gain_new = self.pga_gain
+        self.v_ref = 1.82
+        self.amp_data = []
+        self.ph_data = []
+        self.batt_data = []
+        self.cv_voltage = []
+        self.cv_data = []
+        self.char_list = [self.CHAR1,self.CHAR2,self.CHAR3,self.CHAR4,self.CHAR5]
+        self.file_loc =""
+        self.data_buffer =[['CA', 'pH']]
+
+    def update_char_list(self):
+        self.char_list = [self.CHAR1,self.CHAR2,self.CHAR3,self.CHAR4,self.CHAR5]
+
+    def parse_info(self,packet):
+        self.id = int(packet[0])
+        self.dev_mode = dev_modes[int(packet[2]>>4)]
+        self.pstat_mode = pstat_modes[int(packet[2] & 0x0F)]
+        self.bias = (int(packet[3])-128)/100
+        self.r_tia = rtia_values[int(packet[4])]
+        self.period = period_values[int(packet[5])]
+
+    def parse_data(self, packet, mode):
+        if self.pstat_mode == 'CA_PH':
+            for i in range(0, 80, 4):
+                data = packet[i:i + 4]
+                if (i/4)%2 == 0: #even packets contain pH data
+                    self.amp_data.append(hex2float(data[::-1].hex()))
+                else:
+                    self.ph_data.append(hex2float(data[::-1].hex()))
+                    self.data_buffer.append([self.amp_data[-1], self.ph_data[-1]])
+
+        elif self.pstat_mode == "CV":
+            pass
+
+    def add_batt(self, data):
+        self.batt.append(data)
+
+    def set_devmode(self, input):
+        if input in dev_modes:
+            self.dev_mode = input
+            return 1
+        else:
+            print("Invalid device mode.")
+            return 0
+
+    def set_pstatmode(self, input):
+        if input in pstat_modes:
+            self.pstat_mode_new = input
+            return 1
+        else:
+            print("Invalid potentiostat mode.")
+            return 0
+
+    def set_bias(self, input):
+        if (abs(input) < 1.28):
+            self.bias_new = input
+        else:
+            print("Invalid bias, bias must be between -1.27 and 1.27")
+
+    def set_rtia(self, input):
+        if input in rtia_values:
+            self.r_tia_new = input
+            return 1
+        else:
+            print("Invalid RTIA value.")
+            return 0
+
+    def set_period(self, input):
+        if input in period_values:
+            self.period_new = input
+            return 1
+        else:
+            print("Invalid period value.")
+
+    def set_pga(self, input):
+        if input in pga_values:
+            self.pga_new = input
+            return 1
+        else:
+            print("Invalid PGA value.")
+            return 0
+
+    def get_sender(self, sender):
+        if sender in self.char_list:
+            return self.char_list.index(sender)
+
+    def gen_cmd_str(self):
+        return '0000' + cnv_modes(self.dev_mode_new, self.pstat_mode_new) + cnv_bias(self.bias) + cnv_tia(self.r_tia) + str(hex(self.period))[2:].zfill(2) + cnv_pga(self.pga_gain)
+
+    def set_file_info(self,path,file):
+        self.file_loc = ''.join((path, '/', file, time.strftime("%Y%m%d-%H%M%S"), '.csv'))
+
+    def save_data(self):
+        with open(self.file_loc, 'a', newline='') as csvfile:
+            datawriter = csv.writer(csvfile, delimiter=',')
+            datawriter.writerows(self.data_buffer)
+            self.data_buffer = []
+
+
+def cnv_modes(dev_mode, pstat_mode):
+    try:
+        dev_str = str(dev_modes.index(dev_mode))
+    except ValueError:
+        print("Value error, Invalid device mode.")
+    try:
+        pstat_str = str(pstat_modes.index(pstat_mode))
+    except ValueError:
+        print("Invalid pstat mode.")
+    return dev_str + pstat_str
+
+
+def cnv_bias(bias):
+    return str(hex(int(bias*100)+128)[2:]).zfill(2)
+
+
+def cnv_tia(rtia):
+    try:
+        tia_str = str(hex(rtia_values.index(rtia)))[2:].zfill(2)
+    except ValueError:
+        print("Invalid pstat mode.")
+    return tia_str
+
+def cnv_pga(pga):
+    try:
+        pga_str = str(hex(pga_values.index(pga)))[2:].zfill(2)
+    except ValueError:
+        print("Invalid  PGA.")
+    return pga_str
+
+
+def twos_complement(value, bitLength):
+    return hex(value & (2 ** bitLength - 1))
+
+
+def hex2float(s):
+    i = int(s, 16)                   # convert from hex to a Python int
+    cp = pointer(c_int(i))           # make this into a c integer
+    fp = cast(cp, POINTER(c_float))  # cast the int pointer to a float pointer
+    return fp.contents.value         # dereference the pointer, get the float
+
+
+
+class HETWindow(QtWidgets.QMainWindow):
+    #TODO add characteristic and packet printouts
+    def __init__(self, *args, **kwargs):
+        super(HETWindow, self).__init__(*args, **kwargs)
+        # Load the UI Page
+        uic.loadUi('HET2_Gui.ui', self)  # From QTDesigner
+        self.plotWidget.addLegend()
+
+        self.connectButton.clicked.connect(self.get_device)  # Connect button
+        self.actionQuit.triggered.connect(self.close) # File->Quit
+
+        self.connect_button = 0
+        self.update_button = 0
+        self.start_button = 0
+        self.stop_button = 0
+        self.device_name = "None"
+        self.plot_mode = "CA_PH"
+        self.set_plotmode(self.plot_mode)
+        self.filepath = getcwd()
+        self.filename = "TestExperiment"
+
+    def set_plotmode(self,new_mode):
+        if new_mode == "CA_PH":
+            self.plot_mode == "CA_PH"
+            self.plotWidget.setLabel(axis="left", text="Current (mA)")
+            self.plotWidget.setLabel(axis="bottom", text="Samples")
+            self.plot1 = self.plotWidget.plot([], name='CA Data (mA)', pen='y')
+            self.plot2 = self.plotWidget.plot([], name='pH Data (V)', pen='m')
+            # TODO add second axes for pH
+        elif new_mode == "CV":
+            self.plot_mode == "CV"
+            self.plotWidget.setLabel(axis="left", text = "Current (mA)")
+            self.plotWidget.setLabel(axis="bottom", text="Bias Voltage (V)")
+            self.plot1 = self.plotWidget.plot([], name='Chronoamp Data (mA)', pen='y')
+            self.plot2 = self.plotWidget.plot([], name='pH Data (V)', pen='m')
+
+    def update_plot(self, het):
+        self.update_info(het)
+        if self.plot_mode == "CA_PH":
+            self.plot1.setData(het.amp_data)
+            self.plot2.setData(het.ph_data)
+        elif self.plot_mode == "CV":
+            self.plot1.setData(het.cv_voltage, het.cv_data)
+            self.plot2.setData([])
+
+    def get_device(self):
+        """Connect button press callback, retrieves device name from text box and sets flag"""
+        self.connect_button = 1
+        self.device_name = self.deviceEntry.text()
+
+    def get_filepath(self):
+        self.filepath = QFileDialog.getExistingDirectory(self,"Select folder for saving data...", options=QFileDialog.DontUseNativeDialog)
+        self.filepathDisp.setText(self.filepath)
+        self.filename = self.expEntry.text()
+
+    def update_settings(self):
+        self.update_button = 1
+
+    def start_requested(self):
+        self.start_button = 1
+
+    def stop_requested(self):
+        self.stop_button = 1
+
+    def button_ack(self,button):
+        """Clear button press flag"""
+        if button == "connect":
+            self.connect_button = 0
+        elif button == "update":
+            self.update_button = 0
+        elif button == "start":
+            self.start_button = 0
+        elif button == "stop":
+            self.stop_button = 0
+
+    def display_status(self, msg):
+        """Display messages"""
+        self.statusDisp.setText(msg)
+
+    def update_info(self,het):
+        self.devNumDisp.setText(str(het.id))
+        self.devStatusDisp.setText(het.dev_mode)
+        self.devModeDisp.setText(het.pstat_mode)
+        self.devBiasDisp.setText(str(het.bias))
+        self.devGainDisp.setText(het.r_tia)
+        self.devPeriodDisp.setText(str(het.period))
+
